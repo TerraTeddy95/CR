778bd8f1a917df79c72f8b31de6fbf0eb98b9464
TerraTeddy95 <aktualnienieaktualny@wp.pl>
2015-08-28 08:16:30 +0200

    Update cfg.cs

--- cfg.cs
+++ cfg.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using TShockAPI;
@@ -8,10 +8,18 @@ namespace CodeReward
 {
     public class Config
     {
+        public bool twotimesblock = true;
+
         public int BuffTime = 300;
         public int Interval = 30;
         public int lengthCode = 8;
         public string letters = "0123456789abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM";
+        public string newCode = " Wygenerowano nowy kod: %kod%";
+        public string winMessage = " Zwyciezca: %gracz%";
+        public string LoginIn = " Musisz byc zalogowany aby moc wpisywac kod!";
+        public string Muted = " Jestes zmutowany! nie mozesz wpisywac kodu!";
+        public string TwoTimes = " Ostatnio ty byles pierwszy! Daj szanse innym!";
+
         public List<int> RewardsBuffs = new List<int> { 1, 2, 3, 4, 5 };
         public List<int> RewardsItems = new List<int> { 1, 2, 3, 4, 5 };
 
