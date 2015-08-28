1613616ec9c9a1759d9832b23890efc494308ea2
TerraTeddy95 <aktualnienieaktualny@wp.pl>
2015-08-28 08:15:49 +0200

    Update Chat.cs

--- Chat.cs
+++ Chat.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,45 +21,57 @@ namespace CodeReward
             }
             if (varslist.var.codeon == true)
             {
-                    String sendername = TShock.Players[e.Who].Name;
-                    TSPlayer sender = TShock.Players[e.Who];
+                String sendername = TShock.Players[e.Who].Name;
+                TSPlayer sender = TShock.Players[e.Who];
 
-                    
 
-                    if (e.Text == varslist.var.code)
+
+                if (e.Text == varslist.var.code)
+                {
+                    if (sender.IsLoggedIn == false)
                     {
-                        if (varslist.var.lastp == sendername)
-                        {
-                            sender.SendMessage("[CodeReward] Ostatnio Ty byles pierwszy, daj szanse innym.", Color.Silver);
-                            return;
-                        }
-                        if (sender.mute == true)
-                        {
-                            sender.SendMessage("[CodeReward] Jestes zmutowany, nie mozesz wpisywac kodu.", Color.Silver);
-                            return;
-                        }
-                        if (sender.IsLoggedIn == false)
-                        {
-                            sender.SendMessage("[CodeReward] Musisz byc zalogowany aby moc pisac kod.", Color.Silver);
-                            return;
-                        }
-                        varslist.var.code = null;
-                        varslist.var.codeon = false;
-
-                        foreach (int reward in varslist.var.RewardsBuffs)
-                        {
-                            sender.SetBuff(reward, varslist.var.BuffTime, true);
-                        }
-                        foreach (int reward in varslist.var.RewardsItems)
-                        {
-                            sender.GiveItem(reward, "", 0, 0, 1, 0);
-                        }
-
-
-                        TSPlayer.All.SendMessage("[CodeReward] Zwyciezca: " + sendername, Color.Silver);
-
+                        sender.SendMessage("[CodeReward]" + varslist.var.LoginIn, Color.Silver);
+                        return;
                     }
-                
+                    if (sender.mute == true)
+                    {
+                        sender.SendMessage("[CodeReward]" + varslist.var.Muted, Color.Silver);
+                        return;
+                    }
+                    if (varslist.var.lastp == sendername)
+                    {
+                        if (varslist.var.twotimesblock == true)
+                        {
+                            sender.SendMessage("[CodeReward]" + varslist.var.TwoTimes, Color.Silver);
+                            return;
+                        }
+                    }
+
+                    varslist.var.lastp = sendername;
+                    varslist.var.code = null;
+                    varslist.var.codeon = false;
+
+                    foreach (int reward in varslist.var.RewardsBuffs)
+                    {
+                        sender.SetBuff(reward, varslist.var.BuffTime, true);
+                    }
+                    foreach (int reward in varslist.var.RewardsItems)
+                    {
+                        sender.GiveItem(reward, "", 0, 0, 1, 0);
+                    }
+
+                    string message = varslist.var.winMessage;
+                    while (message.Contains("%gracz%"))
+                    {
+                        message = message.Replace("%gracz%", sendername);
+                    }
+
+
+                    TSPlayer.All.SendMessage("[CodeReward]" + message, Color.Silver);
+
+
+                }
+
 
 
             }
@@ -72,21 +84,26 @@ namespace CodeReward
 
                 if (e.Text == varslist.var.code2)
                 {
-                    if (varslist.var.lastp == sendername)
+                    if (sender.IsLoggedIn == false)
                     {
-                        sender.SendMessage("[CodeReward] Ostatnio Ty byles pierwszy, daj szanse innym.", Color.Silver);
+                        sender.SendMessage("[CodeReward]" + varslist.var.LoginIn, Color.Silver);
                         return;
                     }
                     if (sender.mute == true)
                     {
-                        sender.SendMessage("[CodeReward] Jestes zmutowany, nie mozesz wpisywac kodu.", Color.Silver);
+                        sender.SendMessage("[CodeReward]" + varslist.var.Muted, Color.Silver);
                         return;
                     }
-                    if (sender.IsLoggedIn == false)
+                    if (varslist.var.lastp == sendername)
                     {
-                        sender.SendMessage("[CodeReward] Musisz byc zalogowany aby moc pisac kod.", Color.Silver);
-                        return;
+                        if (varslist.var.twotimesblock == true)
+                        {
+                            sender.SendMessage("[CodeReward]" + varslist.var.TwoTimes, Color.Silver);
+                            return;
+                        }
                     }
+
+                    varslist.var.lastp = sendername;
                     varslist.var.code2 = null;
                     varslist.var.codeon2 = false;
 
@@ -100,7 +117,14 @@ namespace CodeReward
                     }
 
 
-                    TSPlayer.All.SendMessage("[CodeReward] Zwyciezca: " + sendername, Color.Silver);
+                    string message = varslist.var.winMessage;
+                    while (message.Contains("%gracz%"))
+                    {
+                        message = message.Replace("%gracz%", sendername);
+                    }
+
+
+                    TSPlayer.All.SendMessage("[CodeReward]" + message, Color.Silver);
 
                 }
 
